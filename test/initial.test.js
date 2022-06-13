2f76c712e05cbc29add56c4171925624a2f79e30
pattisoj <joshpattison23@icloud.com>
2022-06-13 18:09:18 +0100

    First tests for the initial function

--- test/initial.test.js
+++ test/initial.test.js
@@ -1,1 +1,32 @@
-//Here I will import the function and set tests for it.
+const initial = require("../lib/initial.js");
+
+describe("initials", () => {
+  it("Returns a string", () => {
+    const actual = initial("Type Test");
+    expect(typeof actual).toBe("string");
+  });
+
+  it.each([
+    ["D", "Dave"],
+    ["m", "mary"],
+    ["JP", "Josh Pattison"],
+    ["Js", "John smith"],
+    ["lD", "lucy Davies"],
+    ["mlk", "martin luther king"],
+    ["MEA", "Minos Evalyn Aristide"],
+    ["nLO", "narayan Leena Oda"],
+    ["AmA", "Allan mokhmad Amadi"],
+    ["SALH", "Sotos Augustina Larry Holtz"],
+    ["GmlM", "Girish mahdi leia MacAngus"],
+    ["dbtc", "dragoljub bakarne tressach cardona"],
+    ["jArk", "julia Alba robert karppinen"],
+    ["LFMw", "LI FaBIanA MaRijN wiBOwo"],
+    ["BhAP", "BeRTA hAMzA AyokUnLE PaqUet"],
+  ])(
+    "initial should return %s when %s is submitted with no extra parameters",
+    (expected, givenText) => {
+      const actual = initial(givenText);
+      expect(actual).toEqual(expected);
+    }
+  );
+});
