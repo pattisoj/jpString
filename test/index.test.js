c31aea4233dcbbc1bb7b44f00a08057f416e1a79
pattisoj <joshpattison23@icloud.com>
2022-06-15 17:23:15 +0100

    Added index test file

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const indexObject = require("../index.js");
+
+describe("index object", () => {
+  test("IndexObject is an object", () => {
+    expect(typeof indexObject).toBe("object");
+  });
+});
