b50fa1475424f5301e7a181db1279132db8d910c
pattisoj <joshpattison23@icloud.com>
2022-06-20 21:04:47 +0100

    Added tests and test coverage script

--- test/randomUpperLetter.test.js
+++ test/randomUpperLetter.test.js
@@ -1,1 +1,13 @@
 const randomUpperLetter = require("../lib/randomUpperLetter.js");
+
+describe("random upper letter", () => {
+  test("returns string", () => {
+    const actual = randomUpperLetter();
+    expect(typeof actual).toBe("string");
+  });
+
+  test("returns string of length 1", () => {
+    const actual = randomUpperLetter();
+    expect(actual.length).toEqual(1);
+  });
+});
