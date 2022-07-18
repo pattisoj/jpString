19e7aef8368d6613c781b918991f13f52e18c584
pattisoj <joshpattison23@icloud.com>
2022-07-18 10:18:46 +0100

    Added tests to the jpString object checking the new function is there

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,21 @@
+const {
+  randomLowerLetter,
+  initial,
+  randomUpperLetter,
+} = require("../index.js");
 const indexObject = require("../index.js");
 
 describe("index object", () => {
   test("IndexObject is an object", () => {
     expect(typeof indexObject).toBe("object");
+  });
+  test("IndexObject contains initial function", () => {
+    expect(indexObject.initial).toEqual(initial);
+  });
+  test("IndexObject contains randomUpperLetter", () => {
+    expect(indexObject.randomUpperLetter).toEqual(randomUpperLetter);
+  });
+  test("IndexObject contains randomLowerLetter", () => {
+    expect(indexObject.randomLowerLetter).toEqual(randomLowerLetter);
   });
 });
