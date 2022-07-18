7e66380961a131ca0f170db07af0372113078a50
pattisoj <joshpattison23@icloud.com>
2022-07-18 10:04:08 +0100

    feat: added initial to jpString object

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const initial = require("./lib/initial.js");
 const randomUpperLetter = require("./lib/randomUpperLetter.js");
+const randomLowerLetter = require("./lib/randomLowerLetter.js");
 
 const jpString = {
   initial,
   randomUpperLetter,
+  randomLowerLetter,
 };
 
 module.exports = jpString;
